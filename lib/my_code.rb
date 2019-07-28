88eb7ad9c6b093af69ef270ec3b3da1909f04dd0
Benny Louie <bennylouie1412@gmail.com>
2019-07-28 18:19:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,13 +47,15 @@ end
 def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.size do
-    if source_array[i] == false
-      return false
-    end
-    i += 1 
+    if source_array[i] == true
+      i += 1
+    end 
     return true
+    elsif source_array[i] == false 
+    return false
   end
 end
+      
       
   
   

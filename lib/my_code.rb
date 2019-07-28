c4d7e580ceb74029d1d1ddd1742ed871298d4a38
Benny Louie <bennylouie1412@gmail.com>
2019-07-28 17:56:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,12 +35,20 @@ end
 
 def reduce_to_total(source_array, starting_point)
   i = 0 
-  running_point = starting_point
+  running_point = 0
+  if starting_point
+    running_point = starting_point
     while i < source_array.size do
       running_point += source_array[i]
       i += 1
+    return running_point
   end
-  return running_point
+  while i < source_array.size do
+    running_point += source_array[i]
+    i += 1
+    return running_point
+  end
+end
 end
   
   
